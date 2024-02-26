6ef8b8192af8fe906a5d90ec56605b7c5899d501
Nephew20 <cofieldk20@gmail.com>
2024-02-26 15:14:02 -0500

    added questions and turned answers into buttons:

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 var containerEl = document.querySelector('#container');
 var questionsContainer = document.createElement('ol');
-
+var startButton = document.querySelector('.start-btn')
 var multiChoice = [
     {
         question: "Who is the civil rights leader from Atlanta, GA?",
@@ -18,19 +18,27 @@ var multiChoice = [
 
 console.log(multiChoice[1].question)
 
+//for loop for multiple choice questions in the array
 for (i = 0; i < multiChoice.length; i++) {
     // Creates the question element for each question in the array
     const questionsEl = document.createElement('li');
     questionsEl.textContent = multiChoice[i].question
 
+    //for loop for creating buttons for the answers 
     for (x=0; x<multiChoice[i].ans.length; x++) {
      const answersEl = document.createElement('button')
      answersEl.textContent = multiChoice[i].ans[x]
+     
+     // Appends answeres to the question array 
      questionsEl.appendChild(answersEl)
     }
-
+    // Appends questions to the container
     questionsContainer.appendChild(questionsEl)
     
 }
 
 containerEl.appendChild(questionsContainer)
+
+function gamestart() {
+
+}
